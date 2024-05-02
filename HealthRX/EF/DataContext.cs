eb69032d3e079c43ab9f87feec1c3ff544f7f8aa
azizul-hakim697 <mahkshourov1997@gmail.com>
2024-05-02 23:10:13 +0600

    first commit

---
+++ HealthRX/EF/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using HealthRX.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthRX.EF
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Catagory> Catagories{ get; set; }
+    }
+}
